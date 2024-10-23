2170baba0f21700f13eb7cf63f6f2b0ea2dd774e
aron k koech <varokoaruna81@gmail.com>
2024-10-23 11:59:34 +0300

    writes a function to test for palindrome

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  let testWord = word.toString().replace(/\s+/g, '').toLowerCase();
+  let rev = testWord.split('').reverse().join('');
+  
+  return rev === testWord;
 }
 
+console.log(121);
+console.log('love');
 /* 
-  Add your pseudocode here
+  split the string,reverse and join
+  boolean to check the string if it is true or false
+  return true or false
 */
 
 /*
-  Add written explanation of your solution here
+  the function will take in the string ,split it, remove whitespaces,convert it to lowercase,
+  then join and compare it with the original string
 */
 
 // You can run `node index.js` to view these console logs
